c826b596102243d49c7afec03657e768d06f5a01
Todd Pickell <toddpickell@me.com>
2012-09-27 12:01:22 -0500

    added black box for Eliz item class

---
+++ ItemClassBox/ItemClassBlackBox/ItemClassBlackBox/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  ItemClassBlackBox
+//
+//  Created by Todd Pickell on 9/27/12.
+//  Copyright (c) 2012 Todd Pickell. All rights reserved.
+//
+
+#include <iostream>
+#include "item.h"
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
